0759c4d3401fbd86c5353a56c6363f68991b3fa2
sophi-hub <alexconner470@gmail.com>
2025-03-04 20:23:38 -0300

    Starting the styles. BEM, logo and button switch

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src", "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src", "views"));
 app.use(express.urlencoded({ extended: true }));
